c8bcbf650bf323dbd01a8c8dd0eb526de8a872a4
boniface michael <boniface.michael@thegiggroupng.com>
2019-12-02 17:16:59 +0100

    Added Product Entity And Repository

---
+++ Shop/Shop.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Entities
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Shop/Shop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,79 @@
+﻿using Shop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shop.DataAccess.InMemory
+{
+    class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+
+            if (products is null)
+            {
+                products = new List<Product>();
+            }
+
+            
+        }
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product product)
+        {
+            products.Add(product);
+        }
+        public void Update(Product product)
+        {
+            var productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate is null)
+            {
+                throw new Exception("Product not found");
+            }
+            else
+            {
+                productToUpdate = product;
+            }
+        }
+        public IEnumerable<Product> Products()
+        {
+            return products.ToList();
+        }
+        public Product GetProduct(string id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if(product is null)
+            {
+                throw new Exception("Product Not Found");
+            }
+            else
+            {
+                return product;
+            }
+        }
+        public void Delete(string id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if(product is null)
+            {
+                throw new Exception("Product not found");
+            }
+            else
+            {
+                products.Remove(product);
+            }
+        }
+
+    }
+}
